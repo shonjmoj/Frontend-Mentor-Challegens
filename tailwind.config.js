f6750c4ae546926bd661ec0dc5df1e2161cc8fe4
Elmehdi Ounejjar <elounejj@e1r5p15.1337.ma>
2022-03-14 14:10:20 +0100

    responsive layout

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "middle-row": "#016972",
+        "last-row": "#00403F",
+        "first-row": "#E28425"
+      }
+    },
+  },
+  plugins: [],
+}
