e5730d356bd11ed5b31d8423599b9daee5b8a81e
Elmehdi Ounejjar <elounejj@e1r5p15.1337.ma>
2022-03-15 18:12:29 +0100

    advice-generator challenge

---
+++ advice-generator/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        green: "#50FEAA",
+        lightgrey: "#303B49",
+        background: "#202632"
+      }
+    },
+  },
+  plugins: [],
+}
