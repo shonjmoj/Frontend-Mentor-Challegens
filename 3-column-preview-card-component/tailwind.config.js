8a200b19fab0387abf80a0250c460938d33ca6c9
Elmehdi Ounejjar <elounejj@e1r5p15.1337.ma>
2022-03-16 17:57:16 +0100

    3 column preview card component challenge

---
+++ 3-column-preview-card-component/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'first-row': '#E38724',
+        'middle-row': "#006973",
+        'last-row': "#00403E",
+      }
+    },
+  },
+  plugins: [],
+}
