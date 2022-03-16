f6d50f5c48b5ed699920ccb011e7b4abc7cd2f41
Elmehdi Ounejjar <elounejj@e1r5p15.1337.ma>
2022-03-16 17:46:57 +0100

    profile card component challenge

---
+++ profile-card-component/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        backgrd: "#1EA5B3",
+        card: "#43CBD5",
+        Darkcyan: "hsl(185, 75%, 39%)",
+        Darkblue: "hsl(229, 23%, 23%)",
+        Darkgrayish: "hsl(227, 10%, 46%)"
+      }
+    },
+  },
+  plugins: [],
+}
