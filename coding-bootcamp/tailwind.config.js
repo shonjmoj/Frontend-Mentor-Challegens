07ce4327d1607847282ea9396e9bafa5feee808f
Elmehdi Ounejjar <elounejj@e1r5p15.1337.ma>
2022-03-16 19:07:25 +0100

    adding recources(colors/svgs/imgs) for each challenge

---
+++ coding-bootcamp/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      color: {
+        Darkblue: "hsl(240, 38%, 20%)",
+        grayish: "hsl(240, 18%, 77%)"
+      }
+    },
+  },
+  plugins: [],
+}
