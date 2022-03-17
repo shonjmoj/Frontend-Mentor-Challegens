27ade42300c7edbca460efeafd9dd41aeb51c902
Elmehdi Ounejjar <elounejj@e1r5p15.1337.ma>
2022-03-17 14:56:22 +0100

    time tracking dashboard

---
+++ time-tracking-dashboard/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        verydarkblue: "#0D1323",
+        'darkblue': "#1F1F4B",
+        'lightblue': "#5646E8",
+        'softorange': "#FD8A62",
+        'softblue': "#57C3E6",
+        'softpink': "#FE5F7C",
+        'softgreen': "#4ACF83",
+        'softpurple': "#7334D3",
+        'softyellow': "#F0C75A",
+      }
+    },
+  },
+  plugins: [],
+}
