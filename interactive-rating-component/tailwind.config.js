6b241c190ea45b1daf7553644ee1648b9f5426f2
Elmehdi Ounejjar <elounejj@e1r5p11.1337.ma>
2022-03-21 17:49:55 +0100

    interactive rating component challenge

---
+++ interactive-rating-component/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        verydarkblue: "hsl(216, 12%, 8%)",
+        darkblue: "hsl(213, 19%, 18%)",
+        Orange: "hsl(25, 97%, 53%)"
+      }
+    },
+  },
+  plugins: [],
+}
