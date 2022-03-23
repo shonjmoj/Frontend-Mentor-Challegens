000d0084bfe1b2c197106803fa803f655551d251
Elmehdi Ounejjar <elounejj@e1r5p15.1337.ma>
2022-03-23 20:02:48 +0100

    huddle langing page challenge

---
+++ huddle-landing-page/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      backgrounds: {
+        mobile: "url('/Frontend-Mentor-Challegens/huddle-landing-page/huddle-landing-page-with-single-introductory-section-master/images/bg-mobile.svg')"
+      }
+    },
+  },
+  plugins: [],
+}
